d82d76baae7d8d0eb06ddbfbb8e24f6870506065
SelinaLi <selinali@bu.edu>
2024-10-17 23:13:03 -0400

    Clean file structure and ready for main push

--- docker-entrypoint-initdb.d/init.sql
+++
@@ -1,8 +0,0 @@
--- Create additional users and grant privileges
-CREATE USER 'selina'@'%' IDENTIFIED BY 'snowBall';
-
--- Grant all privileges on the movie_data database to the user
-GRANT ALL PRIVILEGES ON movie_data.* TO 'selina'@'%';
-
--- Apply the changes
-FLUSH PRIVILEGES;
--- my_project/analysis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class analysisConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'analysis'
